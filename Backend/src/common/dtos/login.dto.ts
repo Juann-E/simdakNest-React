b762c972ecb3317b71fb8a0b620ef4faca134177
Juannito Eryadi <134674678+Juann-E@users.noreply.github.com>
2025-08-17 14:11:22 +0700

    update comment

--- Backend/src/common/dtos/login.dto.ts
+++ Backend/src/common/dtos/login.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class LoginDto {
   @IsNotEmpty()
   @IsString()
-  username: string; // bisa username atau email
+  username: string; // bisa username atau email (ganti 1 ganti semua)
 
   @IsNotEmpty()
   @IsString()
